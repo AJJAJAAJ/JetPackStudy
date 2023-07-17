db9444d5d81054f99c0cb26aaf2500de36c72ca7
AnJin <aj_ja2157@126.com>
2023-07-17 23:19:24 +0800

    <feat>(7/17):常用控件

--- app/src/main/java/com/anjin/jetpack/MainActivity.java
+++ app/src/main/java/com/anjin/jetpack/MainActivity.java
@@ -1,14 +1,62 @@
 package com.anjin.jetpack;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.anjin.jetpack.view.SecondActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "myTag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: ");
+        Button login = findViewById(R.id.login);
+        login.setOnClickListener(v -> {
+            startActivity(new Intent(this, SecondActivity.class));
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
     }
 }
---
+++ app/src/main/java/com/anjin/jetpack/view/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.anjin.jetpack.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.anjin.jetpack.R;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
